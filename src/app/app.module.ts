6597b63cc536f1aa62cb5441d4bbb1e1da8b7c51
Fikri <fuadfikri01@gmail.com>
2019-04-17 08:41:19 +0700

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
 import { MenuSampingComponent } from './menu-samping/menu-samping.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuSampingComponent
+    MenuSampingComponent,
+    NavbarComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
