dc5ea24b7cc7d8f8caabb771be8e520bc0238924
Fikri <fuadfikri01@gmail.com>
2019-04-17 08:54:07 +0700

    profile, rekening, transfer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { MenuSampingComponent } from './menu-samping/menu-samping.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { RekeningComponent } from './rekening/rekening.component';
+import { TransferComponent } from './transfer/transfer.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuSampingComponent,
     NavbarComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    RekeningComponent,
+    TransferComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
